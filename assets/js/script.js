4c3d3f1ef46f6f962a5fec216aba3dada4808a98
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-19 11:01:58 -0500

    retrieve inital data with api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,54 @@
-var searchInput = document.getElementById('city-input')
-var submitForm = document.getElementById('submit')
-var cities = JSON.parse(localStorage.getItem('searchedCities')) ?? []
-var searchedCitiesEl = document.querySelector('.searched-cities')
+var searchInput = document.getElementById("city-input");
+var submitForm = document.getElementById("submit");
+var cities = JSON.parse(localStorage.getItem("searchedCities")) ?? [];
+var searchedCitiesEl = document.querySelector(".searched-cities");
+var apiKey = "b90362aa8df79bdd8a26c1d031b51056";
 
-function search(event){
-    event.preventDefault()
-    cities.push(searchInput.value)
-    localStorage.setItem('searchedCities', JSON.stringify(cities))
-    searchInput.value = ""
+//enter city to search
+function search(event) {
+  event.preventDefault();
+  cities.push(searchInput.value);
+  localStorage.setItem("searchedCities", JSON.stringify(cities));
+  searchInput.value = "";
+  getCities();
 }
 
-function getCities(){
-    for(let i = 0; i < cities.length; i++){
-        var cityBtn = document.createElement('button')
-        cityBtn.classList.add("btn", "btn-dark", "w-100")
-        cityBtn.textContent = cities[i]
-        searchedCitiesEl.appendChild(cityBtn)
-    }
+//creates list of past searched cities
+function getCities() {
+  searchedCitiesEl.innerHTML = "";
+  for (let i = 0; i < cities.length; i++) {
+    var cityBtn = document.createElement("button");
+    cityBtn.classList.add("btn", "btn-dark", "w-100");
+    cityBtn.textContent = cities[i];
+    searchedCitiesEl.appendChild(cityBtn);
+  }
 }
 
-function callCity(event){
-    var element = event.target
-    if(element.matches('button')){
-        console.log(element.textContent)
-    }
+//creates api call when city is clicked from list
+function callCity(event) {
+  var element = event.target;
+  var query;
+  var currentCity = {
+    lat: "",
+    lon: "",
+    name: "",
+  };
+  if (element.matches("button")) {
+    query = element.textContent;
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=${apiKey}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        currentCity.lat = data[0].lat;
+        currentCity.lon = data[0].lon;
+        currentCity.name = data[0].name;
+      });
+  }
 }
 
-getCities()
-searchedCitiesEl.addEventListener('click', callCity)
-submitForm.addEventListener('click', search)
+getCities();
+searchedCitiesEl.addEventListener("click", callCity);
+submitForm.addEventListener("click", search);
